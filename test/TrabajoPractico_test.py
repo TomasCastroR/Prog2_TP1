b6f91921194184f4eecf6b0e77fee2759fb77395
TomasCastro <tomascastrorojas18@gmail.com>
2021-09-08 10:46:17 -0300

    selfs

--- test/TrabajoPractico_test.py
+++ test/TrabajoPractico_test.py
@@ -2,13 +2,13 @@ import unittest
 from TrabajoPractico import leer_entrada, diccionario_localidades, separar_por, descartar
 
 class TestMatching (unittest.TestCase):
-    def test_leer_entrada ():
+    def test_leer_entrada (self):
         pass
-    def test_diccionario_localidades ():
+    def test_diccionario_localidades (self):
         pass
-    def test_separar_por ():
+    def test_separar_por (self):
         pass
-    def test_descartar ():
+    def test_descartar (self):
         pass
 
 if __name__ == '__main__':
