56656316f144362552ab31d372fc994ed3c7c1ca
TomasCastro <tomascastrorojas18@gmail.com>
2021-09-08 14:49:05 -0300

    test de separar_por

--- test/TrabajoPractico_test.py
+++ test/TrabajoPractico_test.py
@@ -22,7 +22,16 @@ class TestMatching (unittest.TestCase):
         for idx in range(len(tests)):
             self.assertEqual(diccionario_localidades(eval(tests[idx])), eval(res[idx]))
     def test_separar_por (self):
-        pass
+        with open("test_separar.txt") as entradaFile:
+            tests = entradaFile.readlines()
+        with open("res_separar.txt") as resFile:
+            res = resFile.readlines()
+        self.assertEqual(len(tests), len(res), "Cantidad de ejemplos y resultados no coincide")
+        for idx in range(len(tests)):
+            if idx % 2 == 0:
+                self.assertEqual(separar_por(eval(tests[idx]), "Edad"), eval(res[idx]))
+            else:
+                self.assertEqual(separar_por(eval(tests[idx]), "Genero"), eval(res[idx]))
     def test_descartar (self):
         pass
 
